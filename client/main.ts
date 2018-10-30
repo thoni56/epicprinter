342b23077e0de549ab72753fcfd583e4e8ebf998
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-30 22:18:04 +0100

    Add buttons for save and clear

--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,9 @@
 import { Template } from 'meteor/templating';
 import { Epics } from '../imports/api/epics';
 import { printEpicToPdf } from './printer';
+import { AutoForm } from 'meteor/aldeed:autoform';
+
+import '../imports/ui/epicList/epicList';
 
 import './main.css';
 import './main.html';
@@ -12,10 +15,29 @@ Template.body.helpers({
 })
 
 Template.body.events({
-    'submit #print': function (event) {
+    'click #print': function (event) {
         event.preventDefault();
 
-        const target = event.target;
-        printEpicToPdf(target.key.value, target.title.value, target.effort.value);
+        if (!AutoForm.validateField("epicEntryForm"))
+            return;
+
+        let key = AutoForm.getFieldValue("key", "epicEntryForm");
+        let title = AutoForm.getFieldValue("title", "epicEntryForm");
+        let effort = AutoForm.getFieldValue("effort", "epicEntryForm");
+
+        printEpicToPdf(key, title, effort);
+    },
+    'click #save': function (event) {
+        event.preventDefault();
+
+        if (!AutoForm.validateField("epicEntryForm"))
+            return;
+
+        let key = AutoForm.getFieldValue("key", "epicEntryForm");
+        let title = AutoForm.getFieldValue("title", "epicEntryForm");
+        let effort = AutoForm.getFieldValue("effort", "epicEntryForm");
+        let color = AutoForm.getFieldValue("color", "epicEntryForm");
+
+        Epics.insert({ key: key, title: title, effort: effort });
     }
 });
