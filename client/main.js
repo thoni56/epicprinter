bea609054e8a86f2dc219458c82a0f41c181a7a0
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-30 12:13:07 +0100

    Create an empty PDF document

--- client/main.js
+++ client/main.js
@@ -1,7 +1,11 @@
+import { jsPDF } from 'jspdf';
+
 import './main.css';
 import './main.html';
 
 Template.body.events({
     'click #print': function (event) {
+        let doc = new jsPDF({orientation: 'landscape'});
+        doc.save('epic.pdf');
     }
 });
