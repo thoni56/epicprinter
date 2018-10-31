14236321308e261d0188944c61288247da861921
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-31 15:28:15 +0100

    Generate random color in rgb/html

--- client/main.ts
+++ client/main.ts
@@ -56,10 +56,55 @@ Template.body.events({
     }
 });
 
-function randomColor(): any {
-    return 'hsla(' +
-        (360 * Math.random()) + ', ' +
-        (25 + 70 * Math.random()) + '% ,' +
-        (85 + 10 * Math.random()) + '%)';
+function randomColor(): string {
+    return rgbToHtml(randomRGB());
 }
 
+function rgbToHtml(rgb: number[]) : string {
+    let [r, g, b] = rgb
+    return "#"+r.toString(16)+g.toString(16)+b.toString(16);
+}
+
+function randomRGB() {
+    return hslToRgb(Math.random(),
+        (25 + 70 * Math.random()) / 100,
+        (85 + 10 * Math.random()) / 100);
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ * 
+ * Copied from https://stackoverflow.com/a/9493060/204658
+ */
+function hslToRgb(h, s, l) : number[] {
+    var r, g, b;
+
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        var hue2rgb = function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
