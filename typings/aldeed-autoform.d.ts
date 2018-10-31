3a9d6d932c07447afdcd8900015aa2ec9fc0ede3
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-31 14:01:12 +0100

    Add validateForm to autoform typings

--- typings/aldeed-autoform.d.ts
+++ typings/aldeed-autoform.d.ts
@@ -4,7 +4,8 @@ declare module 'meteor/aldeed:autoform' {
 
     export namespace AutoForm {
         function getFieldValue(fieldName:string, formId?:string) : any;
-        function validateField(formId:string) : boolean;
+        function validateField(formId:string, fieldName:string, skipEmpty?:boolean): boolean;
+        function validateForm(formId:string) : boolean;
         function resetForm(formId:string, templateInstance?:any) : void;
     }
 
