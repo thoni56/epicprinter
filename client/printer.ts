51345dd314fbf7e296d202880fa481bf6b2f2df3
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2018-11-09 11:54:09 +0100

    Show PDF even in Chrome

--- client/printer.ts
+++ client/printer.ts
@@ -1,8 +1,9 @@
 import { jsPDF } from 'jspdf';
 
+declare var window: any;
 
 export function printEpicToPdf(key: string, title: string, effort: number, color: string) {
-    let doc:jsPDF = new jsPDF({ orientation: 'landscape', units: 'mm', format: 'a4' });
+    let doc: jsPDF = new jsPDF({ orientation: 'landscape', units: 'mm', format: 'a4' });
 
     const marginX = 10;
     const marginY = 10;
@@ -19,27 +20,38 @@ export function printEpicToPdf(key: string, title: string, effort: number, color
 
     // Scale to max width of 300 effort points
     const maxEffortWidth = 300;
-    const scale = (paperWidth-marginX-marginY)/maxEffortWidth;
+    const scale = (paperWidth - marginX - marginY) / maxEffortWidth;
 
     // Color is a HTML RGB string, e.g. #45e922
     const rgb = hexToRgb(color);
     doc.setFillColor(rgb["r"], rgb["g"], rgb["b"]);
-    
+
     while (length > maxEffortWidth) {
-        doc.rect(x, y, maxEffortWidth*scale, height, 'F');
-        doc.line(x, y + height, x + maxEffortWidth*scale, y + height);
+        doc.rect(x, y, maxEffortWidth * scale, height, 'F');
+        doc.line(x, y + height, x + maxEffortWidth * scale, y + height);
         length -= maxEffortWidth;
         y += unit;
     }
-    doc.rect(x, y, length*scale, height, 'F');
-    doc.text([key, title, "Effort: "+effort.toString()], marginX + 5, marginY + 10);
+    doc.rect(x, y, length * scale, height, 'F');
+    doc.text([key, title, "Effort: " + effort.toString()], marginX + 5, marginY + 10);
 
-    doc.save(key+'.pdf');
+    showPDF(doc);
 }
 
-function hexToRgb(hexString:string) {
-    let r = Number("0x"+hexString.substr(1,2));
-    let g = Number("0x"+hexString.substr(3,2));
-    let b = Number("0x"+hexString.substr(5,2));
-    return { r: r, g: g, b: b};
+function showPDF(doc: any) {
+    // Show PDF in new tab, even in Chrome, thanks to https://stackoverflow.com/a/17861611/204658
+
+    var string = doc.output('datauristring');
+    var iframe = "<iframe width='100%' height='100%' src='" + string + "'></iframe>";
+    var x = window.open();
+    x.document.open();
+    x.document.write(iframe);
+    x.document.close();
+}
+
+function hexToRgb(hexString: string) {
+    let r = Number("0x" + hexString.substr(1, 2));
+    let g = Number("0x" + hexString.substr(3, 2));
+    let b = Number("0x" + hexString.substr(5, 2));
+    return { r: r, g: g, b: b };
 }
